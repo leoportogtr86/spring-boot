bd040c239bcc8e5230525b04e3778efd92b9f8ed
leoportogtr86 <leoporto_studio@hotmail.com>
2021-03-02 13:44:49 -0300

    primeiro endpoint ok

---
+++ src/main/java/com/portotech/cursospring/resources/CategoriaResources.java
@@ -0,0 +1,22 @@
+package com.portotech.cursospring.resources;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value= "/categorias")
+public class CategoriaResources {
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public  String listar(){
+
+        return "rest ok";
+
+    }
+
+
+
+}
