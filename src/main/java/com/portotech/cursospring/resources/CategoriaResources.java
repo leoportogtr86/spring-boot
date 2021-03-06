73925608fb0a7b7cc3f33b7b749378472f6c701c
leoportogtr86 <leoporto_studio@hotmail.com>
2021-03-06 08:06:33 -0300

    criando uma classe de categorias, instanciando categorias e expondo em um endpoint uma lista de categorias

--- src/main/java/com/portotech/cursospring/resources/CategoriaResources.java
+++ src/main/java/com/portotech/cursospring/resources/CategoriaResources.java
@@ -1,9 +1,13 @@
 package com.portotech.cursospring.resources;
 
 
+import com.portotech.cursospring.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value= "/categorias")
@@ -11,9 +15,17 @@ public class CategoriaResources {
 
 
     @RequestMapping(method = RequestMethod.GET)
-    public  String listar(){
+    public List<Categoria> listar(){
 
-        return "rest ok";
+
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
 
     }
 
